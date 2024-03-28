23d892e966beae6e08f53608e2c43e6dae2d1880
Farhan ES <87615568+Amthefes@users.noreply.github.com>
2024-03-28 07:37:24 +0530

    Initial project file added

---
+++ Habit Builder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habit Builder
+//
+//  Created by Aiju Eldho Issac on 27/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Habit Builder")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habit Builder/Habit_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_BuilderApp.swift
+//  Habit Builder
+//
+//  Created by Aiju Eldho Issac on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Habit_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
